e2bd4371d3a24b5b54192b3d80deb1d87414f8ff
Martin Hey <martinhey@gmx.de>
2017-10-16 22:41:13 +0200

    #1 added default request params

--- WebUntisNet/Rpc/Types/RpcRequest.cs
+++ WebUntisNet/Rpc/Types/RpcRequest.cs
@@ -5,9 +5,14 @@ namespace WebUntisNet.Rpc.Types
     /// </summary>
     public abstract class RpcRequest
     {
+        public RpcRequest()
+        {
+            @params = new RpcRequestParams();
+        }
+
         public abstract string id { get;  }
         public abstract string method { get;  }
-        public RpcRequestParams @params { get;  }
+        public virtual RpcRequestParams @params { get;  }
         public string jsonrpc => "2.0";
     }
 }
